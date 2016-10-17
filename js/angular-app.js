1d1ce3f7a3533ff5f71af224d1902cd60931138a
tmaex <login@tmaex.de>
2016-10-17 16:35:04 +0200

    Solved #8

--- js/angular-app.js
+++ js/angular-app.js
@@ -134,6 +134,7 @@
     };
 
     this.showDeleteUser = function(user) {
+	  this.loadDetail(user);
       this.deleteSelectedUser = user;
       angular.element('#userDeleteModal').modal('show');
     };
